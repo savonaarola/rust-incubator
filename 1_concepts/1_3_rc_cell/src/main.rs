8ae8bfbf7845ed759875d90dae19e5ed420049de
savonaarola <ibn122123@gmail.com>
2025-07-30 17:34:05 +0300

    task 1.3

--- 1_concepts/1_3_rc_cell/src/main.rs
+++ 1_concepts/1_3_rc_cell/src/main.rs
@@ -1,3 +1,40 @@
+use std::{cell::RefCell, rc::Rc};
+
+
+#[derive(Debug)]
+struct  GlobalStack<T>{
+    stack: Rc<RefCell<Vec<T>>>
+}
+
+impl<T> GlobalStack<T>{
+    fn new() -> Self{
+        GlobalStack { stack: Rc::new(RefCell::new(Vec::new())) }
+    }
+
+    fn push(&self, value: T){
+        self.stack.borrow_mut().push(value);
+    }
+    fn pop(&self) -> Option<T>{
+        self.stack.borrow_mut().pop()
+    }
+    fn len(&self) -> usize{
+        self.stack.borrow().len()
+    }
+}
+impl<T> Clone for GlobalStack<T>{
+    fn clone(&self) -> Self {
+        GlobalStack { stack: Rc::clone(&self.stack) }
+    }
+}
 fn main() {
-    println!("Implement me!");
+    let gs1: GlobalStack<i32> = GlobalStack::new();
+    gs1.push(43);
+    let gs2 = gs1.clone();
+    gs2.push(32);
+    gs2.push(21);
+    gs1.push(13);
+    println!("{}",gs2.pop().unwrap());
+    println!("{}",gs1.pop().unwrap());
+    println!("{}",gs1.len());
+    println!("{:?}",gs1);
 }
