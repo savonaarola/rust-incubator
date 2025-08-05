d83a341865c9eed5142b7625b1072ea4a10a5c7a
savonaarola <ibn122123@gmail.com>
2025-08-05 17:58:17 +0300

    task 1.8

--- 1_concepts/1_8_thread_safety/src/main.rs
+++ 1_concepts/1_8_thread_safety/src/main.rs
@@ -1,3 +1,39 @@
+use std::{cell::Cell, marker::PhantomData, rc::Rc, sync::Arc};
+
+
+
+struct OnlySync{
+
+}
+
+struct OnlySend<T: Send>{
+    cell: Cell<T>,
+}
+struct SyncAndSend ;
+struct NotSyncNotSend<T>{
+    rc: Rc<T>,
+}
+
+
 fn main() {
-    println!("Implement me!");
+    let notshared = NotSyncNotSend{
+        rc: Rc::new(21),
+    };
+    let shared = OnlySend{
+        cell: Cell::new(32),
+    };
+
+    std::thread::spawn({
+        let shared = shared;
+        move || {
+            println!("{:?}", shared.cell);
+        }
+    }).join().unwrap();
+
+    // std::thread::spawn({
+    //     let notshared = notshared;
+    //     move || {
+    //         println!(":?",notshared.rc);
+    //     }
+    // }).join().unwrap();
 }
